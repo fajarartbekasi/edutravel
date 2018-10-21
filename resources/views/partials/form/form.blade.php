845cd7900c98aca7e18c27b761c1a8d4730be300
subhi <fajarbekasieditting@gmail.com>
2018-10-22 02:13:45 +0700

    add validation errors and old data

--- resources/views/partials/form/form.blade.php
+++ resources/views/partials/form/form.blade.php
@@ -2,13 +2,27 @@
 
     <form action="/threads" method="post" >
         @csrf
+        <div class="form-group">
+            
+            <select name="channel_id" id="channel_id" class="form-control">
+
+                <option value=""> Chose youre channel... </option>
+
+                @foreach(App\Channel::all() as $channel)
+                     <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : ''  }}> 
+                         {{ $channel->name }}
+                    </option>
+                @endforeach
+            </select>
+            
+        </div>
         <div class="input-group">
             <div class="input-group-prepend">
                 <div class="input-group-text">
                     <span class="fa fa-share-square"></span>
                 </div>
             </div>
-            <input type="text" name="title" id="" class="form-control" placeholder="What youre problems..">
+            <input type="text" name="title" id="" class="form-control" placeholder="What youre problems.." value="{{ old('title') }}">
             
         </div>
         <br>
@@ -20,7 +34,7 @@
                       placeholder="Tell us what your problem is.."
                        cols="30" rows="10"
             >
-            
+                {{ old('body') }}
             </textarea>
         </div>
         <br>
